43a0e70761f1b19926b012f118aba381a0bfc204
Wahid Ali <88141830+Wahid-Alii@users.noreply.github.com>
2022-12-15 15:26:38 +0500

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <div class="logo">
+      <img alt="Vue logo" src="../../src/assets/logo.svg" height="40" />
+    </div>
+    <div class="links">
+      <ul>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+header {
+  display: grid;
+  grid-template-columns: auto auto;
+  border-bottom: 2px solid #e3ebe7;
+  align-items: center;
+}
+
+.logo{
+    margin: 10px 10px 10px 20px;
+}
+
+ul {
+  display: flex;
+  list-style-type: none;
+  gap: 10px;
+  float: right;
+  margin-right: 20px;
+}
+</style>
